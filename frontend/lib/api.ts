46799164993357dcfba05efbc44b45381e7a7518
cmouns <mounir.sebti33@email.com>
2025-11-12 15:38:34 +0100

    api getProducts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,1 +1,15 @@
-const API_URL = 'https://fakestoreapi.com/products';
+import { Product } from "@/type";
+const API_URL = 'https://fakestoreapi.com';
+// Récupère la liste des produits depuis l'API Fakestore
+const getProducts = async () : Promise<Product[]> => {
+    try {
+        const response = await fetch(`${API_URL}/products`)
+        if(!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    } catch (error) {
+        console.log('Error fetching products', error);
+        throw error;
+    };
+};
