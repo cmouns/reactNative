26e304034a7e8718a25c27668d68e7a241e8a37f
cmouns <mounir.sebti33@email.com>
2025-11-12 14:58:11 +0100

    maj header

--- frontend/components/HomeHeader.tsx
+++ frontend/components/HomeHeader.tsx
@@ -1,32 +1,68 @@
-import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { AppColors } from '@/constants/theme'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Logo from './Logo'
-import { AntDesign } from '@expo/vector-icons'
-import { useRouter } from 'expo-router'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { AppColors } from "@/constants/theme";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Logo from "./Logo";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const HomeHeader = () => {
-    const router = useRouter();
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-            <Logo />
-            <View style={styles.iconContainer}>
-                <TouchableOpacity style={styles.searchButton} onPress={()=> router.push('/search')}>
-                    <AntDesign name="search" size={20} color={AppColors.primary[700]}/>
-                </TouchableOpacity>
+      <View style={styles.header}>
+        <Logo />
+        <View style={styles.iconContainer}>
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => router.push("/(tabs)/search")}
+          >
+            <AntDesign name="search" size={20} color={AppColors.primary[700]} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => router.push("/(tabs)/favorite")}
+          >
+            <MaterialCommunityIcons
+              name="heart-outline"
+              size={20}
+              color={AppColors.primary[700]}
+            />
+            
+            <View style={styles.itemsView}>
+              <Text style={styles.itemsText}> 0 </Text>
             </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => router.push("/(tabs)/cart")}
+          >
+            <MaterialCommunityIcons
+              name="heart-outline"
+              size={20}
+              color={AppColors.primary[700]}
+            />
+            
+            <View style={styles.itemsView}>
+              <Text style={styles.itemsText}> 0 </Text>
+            </View>
+          </TouchableOpacity>
         </View>
-    
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeHeader
+export default HomeHeader;
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     backgroundColor: AppColors.background.primary,
     marginTop: Platform.OS === "android" ? 35 : 0,
   },
@@ -41,19 +77,38 @@ const styles = StyleSheet.create({
   },
   iconContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    alignItems: "center",
+    flexDirection : "row",
+    justifyContent: "flex-end",
   },
-  searchButton : {
+  searchButton: {
     backgroundColor: AppColors.primary[50],
     borderRadius: 5,
     width: 35,
     height: 35,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginLeft: 8,
     borderWidth: 1,
     borderColor: AppColors.primary[500],
-    position: 'relative',
-  }
-})
+    position: "relative",
+  },
+  itemsView: {
+    position: "absolute",
+    top: -5,
+    right: -5,
+    borderRadius: 50,
+    width: 15,
+    height: 15,
+    backgroundColor: AppColors.background.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: AppColors.primary[500],
+  },
+  itemsText: {
+    fontSize: 10,
+    color: AppColors.accent[500],
+    fontWeight: 800,
+  },
+});
