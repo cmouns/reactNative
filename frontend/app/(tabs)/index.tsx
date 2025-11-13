50ef52393600e856fd4b53059778e0db5b561dba
cmouns <mounir.sebti33@email.com>
2025-11-13 15:59:16 +0100

    show product

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -2,18 +2,32 @@
 import HomeHeader from "@/components/HomeHeader";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import Wrapper from "@/components/Wrapper";
+import { AppColors } from "@/constants/theme";
 import { useProductStore } from "@/store/productStore";
 import { Product } from "@/type";
+import { AntDesign } from "@expo/vector-icons";
 import { useEffect, useState } from "react";
-import { Platform, StyleSheet, View, Text } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+} from "react-native";
 // import { HelloWave } from '@/components/hello-wave';
 // import ParallaxScrollView from '@/components/parallax-scroll-view';
 // import { ThemedText } from '@/components/themed-text';
 // import { ThemedView } from '@/components/themed-view';
 // import { Link } from 'expo-router';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import ProductCard from "@/components/ProductCard";
 
 export default function HomeScreen() {
+  const router = useRouter();
   const [featuredProducts, setFeaturedProducts] = useState<Product[]>([]);
   const {
     products,
@@ -36,13 +50,159 @@ export default function HomeScreen() {
     }
   }, [products]);
 
+  const navigateToCategory = (category: string) => {
+    router.push({
+      pathname: "/(tabs)/shop",
+      params: { category },
+    });
+  };
   if (loading) {
-  return (
-    <Wrapper>
-      <LoadingSpinner fullScreen />
-    </Wrapper>
-  );  
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.errorContainer}>
+          <LoadingSpinner fullScreen />
+        </View>
+      </SafeAreaView>
+    );
   }
+
+  if (error) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>Error: {error}</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <View style={styles.wrapper}>
+      <HomeHeader />
+      <View style={styles.contentContainer}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollContainerView}
+        >
+          <View style={styles.categoriesSection}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionTitle}>Catégories</Text>
+            </View>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {categories?.map((category) => (
+                <TouchableOpacity
+                  style={styles.categoryButton}
+                  key={category}
+                  onPress={() => navigateToCategory(category)}
+                >
+                  <AntDesign
+                    name="tag"
+                    size={16}
+                    color={AppColors.primary[500]}
+                  />
+
+                  <Text style={styles.categoryText}>
+                    {category.charAt(0).toUpperCase() + category.slice(1)}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
+          <View style={styles.featuredSection}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionTitle}>Meilleurs Ventes</Text>
+              <TouchableOpacity>
+                <Text style={styles.seeAllText}>Voir tout</Text>
+              </TouchableOpacity>
+            </View>
+            <FlatList
+              data={featuredProducts}
+              keyExtractor={(item) => item.id.toString()}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={styles.featuredProductsContainer}
+              renderItem={({ item }) => (
+                <View style={styles.featuredProductContainer}>
+                  <ProductCard product={item} compact/>
+                </View>
+              )}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: AppColors.background.primary,
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+  },
+  errorText: {
+    fontFamily: "Inter-Medium",
+    fontSize: 16,
+    color: AppColors.error,
+    textAlign: "center",
+  },
+  contentContainer: {
+    paddingLeft: 20,
+  },
+  scrollContainerView: {
+    paddingBottom: 300,
+  },
+  categoryText: {
+    marginLeft: 6,
+    fontFamily: "Inter-Medium",
+    fontSize: 12,
+    color: AppColors.text.primary,
+    textTransform: "capitalize",
+  },
+  categoriesSection: {
+    marginTop: 10,
+    marginBottom: 16,
+  },
+  categoryButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: AppColors.background.secondary,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    borderRadius: 8,
+    marginLeft: 5,
+    minWidth: 100,
+  },
+  sectionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+    paddingRight: 20,
+  },
+  sectionTitle: {
+    fontFamily: "Inter-Medium",
+    fontSize: 14,
+    color: AppColors.primary[500],
+  },
+  wrapper: {
+    flex: 1,
+    backgroundColor: AppColors.background.primary,
+  },
+  featuredProductsContainer : {
+
+  },
+  featuredProductContainer : {
+
+  },
+  seeAllText : {
+    fontFamily: "Inter-Medium",
+    fontSize: 12,
+    color: AppColors.primary[500],
+  }
+});
---
+++ frontend/components/ProductCard.tsx
@@ -0,0 +1,125 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+  StyleProp,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React from "react";
+import { Product } from "@/type";
+import { AppColors } from "@/constants/theme";
+
+interface ProductCardProps {
+  product: Product;
+  compact?: boolean;
+  customStyle?: StyleProp<ViewStyle>;
+}
+const ProductCard: React.FC<ProductCardProps> = ({
+  product,
+  compact = false,
+  customStyle,
+}) => {
+  const { id, title, price, category, image } = product;
+  return (
+    <TouchableOpacity
+      style={[styles.card, compact && styles.compactCard, customStyle]}
+      activeOpacity={0.8}
+    >
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: image }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.category}>{category}</Text>
+        <Text
+          style={styles.title}
+          numberOfLines={compact ? 1 : 2}
+          ellipsizeMode="tail"
+        >
+          {title}
+        </Text>
+        <View style={styles.footer}>
+          <Text style={styles.price}>€{price.toFixed(2)}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ProductCard;
+
+const styles = StyleSheet.create({
+  price: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: AppColors.primary[600],
+    marginBottom: 5,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: AppColors.text.primary,
+    marginBottom: 8,
+  },
+  category: {
+    fontSize: 12,
+    color: AppColors.text.tertiary,
+    textTransform: "capitalize",
+    marginBottom: 4,
+  },
+  content: {
+    padding: 12,
+    backgroundColor: AppColors.background.secondary,
+  },
+  favoriteButton: {
+    position: "absolute",
+    top: 8,
+    right: 8,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 18,
+    padding: 2,
+    width: 32,
+    height: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: AppColors.warning,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    position: "relative",
+    height: 150,
+    backgroundColor: AppColors.background.primary,
+    padding: 5,
+  },
+  compactCard: {
+    width: 150,
+    marginRight: 12,
+  },
+  card: {
+    backgroundColor: AppColors.background.primary,
+    borderRadius: 12,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 2,
+    width: "48%",
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: AppColors.gray[200],
+  },
+});
